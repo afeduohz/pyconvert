73c8293cc445fa7bb57791242887b040ebfcad57
Defa Zhou <zhoudefa666@163.com>
2021-03-24 22:56:50 +0800

    add test cases

---
+++ test/test_converter.py
@@ -0,0 +1,31 @@
+import json
+import unittest
+
+from convert import Converter
+
+
+def pretty_print(o):
+    print(json.dumps(o, indent=2))
+
+
+class TestConverter(unittest.TestCase):
+
+    def test_from_file(self):
+        template = '{</servers>ss=[<>{<>n=(./name)}]datetime=(/time)}'
+        with open('./1.json', 'r') as f:
+            data = json.load(f)
+            conv = Converter(template)
+            dest = conv.convert(data)
+            pretty_print(dest)
+
+    def test_deep_recur(self):
+        template = '{</servers>ss=[<>{<>n=(./name)d=[<./disks>{<>u=(./uuid)t=(./type)clone=(True)}]}]datetime=(/time)}'
+        with open('./2.json', 'r') as f:
+            data = json.load(f)
+            conv = Converter(template)
+            dest = conv.convert(data)
+            pretty_print(dest)
+
+
+if __name__ == '__main__':
+    unittest.main()
